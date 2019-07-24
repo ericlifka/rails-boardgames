e0b2d9882b7b09a88dbc20a54786eed429f16410
keri lawrence <keri.lawrence@inin.com>
2019-07-24 11:30:56 -0400

    add images for games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_172933) do
+ActiveRecord::Schema.define(version: 2019_07_23_194113) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_172933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
